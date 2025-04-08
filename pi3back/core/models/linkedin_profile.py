548a76da8c012ecad305c8cdf32a30a7165bcbe5
João Savietto <joao.savietto@youtan.com.br>
2025-04-07 21:06:45 -0300

    feat: vakires defaukt para as urls do perfil

---
+++ pi3back/core/models/linkedin_profile.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class LinkedInProfile(models.Model):
+    """Stores scraped LinkedIn profile data"""
+    url = models.URLField(max_length=255)
+    about = models.TextField(blank=True, default='Carregando...')
+    name = models.CharField(max_length=255, default='Carregando...')
+    experiences = models.JSONField(default=list)
+    educations = models.JSONField(default=list)
+    interests = models.JSONField(default=list)
+    accomplishments = models.JSONField(default=list)
+    contacts = models.JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"LinkedIn Profile: {self.name}"
+
+    class Meta:
+        ordering = ['name']
