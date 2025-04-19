f9f20f217e05a71039705bb953a14ac9367cdc9e
João Savietto <Joo Savietto>
2025-04-19 01:01:03 -0300

    refactor? flake8

--- pi3back/core/api/views/applicant_views.py
+++ pi3back/core/api/views/applicant_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import status, viewsets
-from rest_framework.response import Response
 
 from pi3back.core.models import Applicant
 from pi3back.core.api.serializers import ApplicantSerializer
@@ -13,9 +12,13 @@ User = get_user_model()
 
 @extend_schema_view(
     list=extend_schema(description="Lista todos os candidatos cadastrados"),
-    retrieve=extend_schema(description="Retorna os detalhes de um candidato específico"),
+    retrieve=extend_schema(
+        description="Retorna os detalhes de um candidato específico"
+    ),
     create=extend_schema(description="Cria um novo candidato"),
-    partial_update=extend_schema(description="Atualiza parcialmente um candidato"),
+    partial_update=extend_schema(
+        description="Atualiza parcialmente um candidato"
+    ),
     destroy=extend_schema(description="Remove um candidato"),
 )
 class ApplicantViewSet(viewsets.ModelViewSet):
@@ -26,11 +29,7 @@ class ApplicantViewSet(viewsets.ModelViewSet):
 
     def _trigger_scraper(self, applicant):
         user = self.request.user
-        if (
-            user.linkedin_user
-            and user.linkedin_password
-            and applicant.url
-        ):
+        if user.linkedin_user and user.linkedin_password and applicant.url:
             scrape_and_update_applicant.delay(
                 linkedin_url_profile=applicant.url,
                 username=user.linkedin_user,
